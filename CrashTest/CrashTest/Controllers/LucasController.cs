58d4d1d0da256fc92dd23163c3c068d03ea8cea9
Lucas DELEFORTRIE <delefortrie.lucas@btssio.local>
2020-02-11 16:00:53 +0100

    Création des tests

---
+++ CrashTest/CrashTest/Controllers/LucasController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CrashTest.Controllers
+{
+    public class LucasController : Controller
+    {
+        // GET: Lucas
+        public ActionResult IndexLucas()
+        {
+            return View();
+        }
+    }
+}
---
+++ CrashTest/CrashTest/Controllers/MaximeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CrashTest.Controllers
+{
+    public class MaximeController : Controller
+    {
+        // GET: Maxime
+        public ActionResult IndexMaxime()
+        {
+            return View();
+        }
+    }
+}
