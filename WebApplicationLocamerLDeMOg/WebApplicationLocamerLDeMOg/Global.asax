8b5897349d1ba0ad1f1f0dce8104bedef212add1
Max Ogier <lolo.max@hotmail.com>
2020-02-11 15:42:44 +0100

    Création du projet

---
+++ WebApplicationLocamerLDeMOg/WebApplicationLocamerLDeMOg/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebApplicationLocamerLDeMOg.MvcApplication" Language="C#" %>
---
+++ WebApplicationLocamerLDeMOg/WebApplicationLocamerLDeMOg/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebApplicationLocamerLDeMOg.Startup))]
+namespace WebApplicationLocamerLDeMOg
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
