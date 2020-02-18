939b5b084ada2a5cd904d57163f464bf73634ddc
Dr-Clank <lolo.max@hotmail.com>
2020-02-18 17:01:36 +0100

    Ajout du calendrier dans le Create du Sejours

--- WebApplicationLocamerLDeMOg/WebApplicationLocamerLDeMOg/Models/Sejour.cs
+++ WebApplicationLocamerLDeMOg/WebApplicationLocamerLDeMOg/Models/Sejour.cs
@@ -11,6 +11,7 @@ namespace WebApplicationLocamerLDeMOg.Models
 {
     using System;
     using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
     
     public partial class Sejour
     {
